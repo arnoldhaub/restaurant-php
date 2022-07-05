e1facc7a1a3731f73561f93ee198a49fd37a407e
Arnold Haubursin <arnoldhaubursin@gmail.com>
2022-07-05 20:01:23 +0200

    end day

---
+++ admin/nav.php
@@ -0,0 +1,31 @@
+      <!-- MENU - début -->
+      <div id="menu" class="row sticky-top bg-dark justify-content-around">
+          <nav class="col-lg-12  navbar navbar-expand-lg px-5 ">
+              <div class="container-fluid">
+                <a class="navbar-brand tex" href="index.php"><img src="img/logo.png" alt="logo"></a>
+                <button class="navbar-toggler bg-white" style="--bs-bg-opacity: .2;" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                  <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                      <a class="nav-link text-secondary" href="/admin/index.php">Accueil</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link text-secondary" href="/admin/messages.php">Messages</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link text-secondary" href="/admin/guest_book.php">Livre d'or</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link text-secondary" href="/admin/gallery.php">Galerie</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-secondary" href="/index.php">Retour au site</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </nav>
+      </div>
+      <!-- MENU - fin -->
